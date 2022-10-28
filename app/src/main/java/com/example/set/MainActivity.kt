cabac9bc8a2b138a9363e00f967dba51e761c9ea
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-10-28 15:56:58 +0530

    Changes Updated

--- app/src/main/java/com/example/set/MainActivity.kt
+++ app/src/main/java/com/example/set/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.set
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btn : AppCompatImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn=findViewById(R.id.redirect)
+
+        btn.setOnClickListener {
+            val intent= Intent(this@MainActivity,SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/set/SettingsActivity.kt
+++ app/src/main/java/com/example/set/SettingsActivity.kt
@@ -1,11 +1,32 @@
 package com.example.set
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 
 class SettingsActivity : AppCompatActivity() {
+    private lateinit var submit: AppCompatImageButton
+    private lateinit var prev_btn:AppCompatImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        submit=findViewById(R.id.submit_btn)
+        submit.setOnClickListener {
+            makeText(applicationContext,"Succssfully Updated",Toast.LENGTH_SHORT).show()
+        }
+
+        prev_btn=findViewById(R.id.back_btn)
+        prev_btn.setOnClickListener {
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
